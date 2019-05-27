19b29f6639d112489cbfcccaab1e0dc39823667a
jay_bellway <jayant.bellway@gmail.com>
2019-05-27 13:18:49 +0530

    Initial Commit.

---
+++ FirebaseChatX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChatX
+//
+//  Created by jayant patidar on 27/05/19.
+//  Copyright © 2019 IntellArc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
